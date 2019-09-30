f6c9f86c970dfa01438fda146c5c76f3ffbde9ad
milankarunarathne <mhkarunarathne@gmail.com>
2019-10-01 01:12:46 +0530

    Adding Static images and static json object as a data object

---
+++ data/data.js
@@ -0,0 +1,44 @@
+export default () => {
+    return[
+            {
+                id: 1,
+                image: 'N22732308A_1.jpg',
+                favourite: true
+            },
+            {
+                id:2,
+                image: 'N23348011A_1.jpg',
+                favourite: false
+            },
+            {
+                id:3,
+                image: 'N24753374A_1.jpg',
+                favourite: true
+            },
+            {
+                id:4,
+                image: 'N27057326A_1.jpg',
+                favourite: true
+            },
+            {
+                id:5,
+                image: 'N28808635A_1.jpg',
+                favourite: false
+            },
+            {
+                id:6,
+                image: 'N28965275V_1.jpg',
+                favourite: true
+            },
+            {
+                id:7,
+                image: 'N29004760A_1.jpg',
+                favourite: true
+            },
+            {
+                id:8,
+                image: 'N29898024A_1.jpg',
+                favourite: false
+            }
+    ]
+}
