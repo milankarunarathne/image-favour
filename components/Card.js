3c62751c6f4dd80f3036e40db7269aeeb7f52985
milankarunarathne <mhkarunarathne@gmail.com>
2019-10-02 22:24:59 +0530

    Update UI view with JSX styles

--- components/Card.js
+++ components/Card.js
@@ -13,30 +13,82 @@ export default class Card extends React.Component {
     }
 
     render() {
-        const { id, image, name, price, likes, discription, hashtags } = this.props.data;
+        const { id, image, name, price, likes, description, hashtags, user, userImage } = this.props.data;
         const isFavourite = this.state.isFavourite;
         return (
             <div style={{ backgroundColor: 'rgba(255, 255, 255, 1)', marginBottom: -15 }}>
-                <img src={`static/art/${image}`} alt="" />
-                <div onClick={() => this.handleClick(id, isFavourite)}>
-                    <IndexButtonBar favourite={isFavourite} />
+                <div className = "userImage">
+                    <p><img src={`static/art/${userImage}`} alt="" /><span><strong>{user}</strong></span></p>    
                 </div>
-                <p>{name}</p>
+                
+                <div className = "itemImage">
+                    <img src={`static/art/${image}`} alt="" />
+                    <div className = "">
+                        <p>{name}</p>
+                        <p><strong>{price}</strong></p>
+                    </div>
+                    <div className = "actionButton" onClick={() => this.handleClick(id, isFavourite)}>
+                        <IndexButtonBar favourite={isFavourite} />
+                    </div>
+                </div>
+                
+                
 
-                <p><strong>{price}</strong></p>
-
-                <p><FaHeart color="#5b34ce"
-                    size="20px"
-                    style={{ margin: '0 5px' }}
-                /><strong>{likes} Likes</strong></p>
-                <p>{discription}</p>
-                <p>{hashtags}</p>
+                
+                <div className = "likesCount">
+                    <p><FaHeart
+                        size="20px"
+                        style={{ margin: '0 5px' }}
+                    /><strong>{likes} Likes</strong></p>
+                </div>
+                <p>{description}</p>
+                <div className = "hashTags">
+                    <p>{hashtags}</p>
+                </div>
+                
                 <style jsx>{`
-                    img{
-                        width: 95%;
+                    .itemImage img{
+                        width: 96%;
                         height: auto;
                         margin: 2%;
                     }
+
+                    .userImage img{
+                        border-radius: 50%;
+                        float: left;
+                        width: 60px;
+                        //height: auto;
+                        margin: 1%;
+                        //margin-top: 1%;
+                        margin-left: 4%;
+                    }
+
+                    .userImage strong{
+                        position: relative;
+                        float: left;
+                        padding-top: 47px;
+                        vertical-align: middle;
+                        //margin-left: 1%;
+                        height: 60px;
+                        top: -12px;
+                        color: #5534b4;
+                    }
+
+                    .likesCount p{
+                        color: #2316bd;
+                    }
+
+                    .likesCount strong{
+                        color: #2316bd;
+                        //vertical-align: middle;
+                    }
+
+                    .hashTags p{
+                        color: #1711bc;
+                    }
+
+
+
                 `}</style>
             </div>
         );
--- data/data.js
+++ data/data.js
@@ -5,80 +5,96 @@ export default () => {
                 name: 'Airpod',
                 price: 'AED 500',
                 likes: 32,
-                description: 'This is good product',
+                description: 'Now with more talk time, voice-activated Siri access — and a new wireless charging case.',
                 hashtags: "#airpods #apple #mobile phones # wireless",
                 image: 'N22732308A_1.jpg',
-                isFavourite: true
+                isFavourite: true,
+                user:"Amusingbuzfuz",
+                userImage: "user_1.jpg"
             },
             {
                 id: 'item_2',
                 name: 'Lady Watch',
                 price: 'AED 100',
                 likes: 22,
-                description: 'This is good product',
+                description: 'Instantly view call, text, app notifications and music in play.',
                 hashtags: "#titan #watch #jewellery # lady watch",
                 image: 'N23348011A_1.jpg',
-                isFavourite: false
+                isFavourite: false,
+                user:"Jameastcoast",
+                userImage: "user_2.jpg"
             },
             {
                 id: 'item_3',
                 name: 'Vaccum Cleaner',
                 price: 'AED 300',
                 likes: 332,
-                description: 'This is good product',
+                description: 'A vacuum cleaner, also known simply as a vacuum.',
                 hashtags: "#home tool #vaccum #electrical item ",
                 image: 'N24753374A_1.jpg',
-                isFavourite: true
+                isFavourite: true,
+                user:"Gregsburyleash",
+                userImage: "user_3.jpg"
             },
             {
                 id: 'item_4',
-                name: 'CTR Game',
+                name: 'PS4',
                 price: 'AED 40',
                 likes: 531,
-                description: 'This is good product',
+                description: 'You\'ve come to the right place. Exclusive games take you on incredible journeys',
                 hashtags: "#game #PS4 #animation",
                 image: 'N27057326A_1.jpg',
-                isFavourite: true
+                isFavourite: true,
+                user:"Unleveledsting",
+                userImage: "user_4.jpg"
             },
             {
                 id: 'item_5',
-                name: 'Earbuds',
+                name: 'Bose Earbuds',
                 price: 'AED 900',
                 likes: 12,
-                description: 'This is good product',
+                description: 'Bose earphones are engineered to give you powerful sound in a lightweight, stable fit.',
                 hashtags: "#earbuds #bose #wireless #handfree",
                 image: 'N28808635A_1.jpg',
-                isFavourite: false
+                isFavourite: false,
+                user:"Mashiecement",
+                userImage: "user_5.jpg"
             },
             {
                 id: 'item_6',
                 name: 'Nike',
                 price: 'AED 200',
                 likes: 23,
-                description: 'This is good product',
+                description: 'Join the greatest team in sports to get the very best of Nike.',
                 hashtags: "#nike #shoes #kids shoes #casual",
                 image: 'N28965275V_1.jpg',
-                isFavourite: true
+                isFavourite: true,
+                user:"Healthfloating",
+                userImage: "user_6.jpg"
             },
             {
                 id: 'item_7',
                 name: 'Samsung Note',
                 price: 'AED 2500',
                 likes: 62,
-                description: 'This is good product',
+                description: 'Get the Galaxy Note10 starting from $18.75/mo for 24 months or $449.99 with eligible trade-in and Samsung Financing.',
                 hashtags: "#phone #samsung #mobile phones #note",
                 image: 'N29004760A_1.jpg',
-                isFavourite: true
+                isFavourite: true,
+                user:"Yummyevolving",
+                userImage: "user_7.jpg"
             },
             {
                 id: 'item_8',
                 name: 'iPhone 11 Pro',
                 price: 'AED 4700',
                 likes: 91,
-                description: 'This is good product',
+                description: 'A transformative triple‑camera system that adds tons of capability without complexity. An unprecedented leap in battery life.',
                 hashtags: "#phone #11pro #mobile phones #latest",
                 image: 'N29898024A_1.jpg',
-                isFavourite: false
+                isFavourite: false,
+                user:"Listfidelity",
+                userImage: "user_8.jpg"
             }
     ]
 }
